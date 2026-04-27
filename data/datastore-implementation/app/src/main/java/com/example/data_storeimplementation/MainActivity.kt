929edbd91a0738c12ac8ec971d1911eee1c28c4a
Max Gonzalez <52800035+MaxKeenti@users.noreply.github.com>
2026-04-27 10:15:15 -0600

    chore: auto-commit after quick-task
    GSD-Unit: Q10

--- data/datastore-implementation/app/src/main/java/com/example/data_storeimplementation/MainActivity.kt
+++ data/datastore-implementation/app/src/main/java/com/example/data_storeimplementation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.data_storeimplementation
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,11 +9,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.data_storeimplementation.ui.theme.DatastoreimplementationTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
 
+val Context.dataStore by preferencesDataStore("Configuración")
+val clave = stringPreferencesKey("ejemplo")
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +41,36 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+fun readLeer (context:Context): Flow<String> {
+    return context.dataStore.data.map {
+        prefs -> prefs[clave] ? : ""
+    }
+    suspend saveGuardar (context:Context, valueValor:String){
+        context.dataStore.edit{
+            prefs -> prefs [clave] = valueValor
+        }
+    }
+}
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ourUI(context:Context) {
+    val scope = rememberCoroutineScope()
+    var valueValor by remember {
+        mutableStateOf("")
+    }
+    val usrFlow = readLeer(context)
+    val usrSave by usrFlow.colectAsState(initial = "")
+
+    //Add here widgets
+
+    TextField(
+        value = valueValor,
+        onValueChange = {
+            valueValor = it
+        },
+        label = {
+            Text("Valor")
+        }
     )
 }
 
